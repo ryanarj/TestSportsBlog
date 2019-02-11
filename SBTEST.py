793115718e681fb2073d2abe651b47aa6e556cc4
ryanarj <karshan813@gmail.com>
2019-02-11 15:10:56 -0500

    Created master Test Class

--- SBTEST.py
+++ SBTEST.py
@@ -5,8 +5,8 @@ import unittest
 class SBTEST(unittest.TestCase):
 
     def get_driver(self):
-        return Driver(
-            'https://stackoverflow.com/questions/39428042/use-selenium-with-chromedriver-on-mac').driver()
+        path = 'http://127.0.0.1:8000/'
+        return Driver(path).driver()
 
 if __name__ == "__main__":
     suite = unittest.defaultTestLoader.loadTestsFromTestCase(SBTEST)
